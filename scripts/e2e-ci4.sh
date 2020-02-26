684b2bb1ebe36fbcc6009b738cfa73957e4e627b
Victor Savkin <mail@vsavkin.com>
2020-02-26 11:17:08 -0500

    feat(core): memoize project source hashing

--- scripts/e2e-ci4.sh
+++ scripts/e2e-ci4.sh
@@ -8,14 +8,8 @@ mkdir -p tmp/nx
 
 export SELECTED_CLI=$1
 jest --maxWorkers=1 ./build/e2e/ng-add.test.js &&
-jest --maxWorkers=1 ./build/e2e/ngrx.test.js &&
 jest --maxWorkers=1 ./build/e2e/node.test.js &&
 jest --maxWorkers=1 ./build/e2e/nx-plugin.test.js &&
 jest --maxWorkers=1 ./build/e2e/print-affected.test.js &&
 jest --maxWorkers=1 ./build/e2e/react.test.js &&
-jest --maxWorkers=1 ./build/e2e/report.test.js &&
-jest --maxWorkers=1 ./build/e2e/run-many.test.js &&
-jest --maxWorkers=1 ./build/e2e/storybook.test.js &&
-jest --maxWorkers=1 ./build/e2e/upgrade-module.test.js &&
-jest --maxWorkers=1 ./build/e2e/web.test.js &&
-jest --maxWorkers=1 ./build/e2e/tasks-runner-v2.test.js
+jest --maxWorkers=1 ./build/e2e/report.test.js
